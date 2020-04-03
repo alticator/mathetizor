e8130d31bcc2d4bc07c0b5ce964cc57fb52973cb
alticator <main@alticator.com>
2020-04-03 18:26:38 +0300

    Version 0.0.1
    1. You can enter two numbers and the program shows the sum, difference, product and quotient of them.
    Note: Average does not work yet

---
+++ app.js
@@ -0,0 +1,67 @@
+// Alticator 2020
+
+document.onkeypress = keyPress;
+
+function keyPress(event) {
+	if (event.key == "Enter") {
+		runCommand();
+	}
+}
+
+var tab = "overview";
+var line = " </br> ";
+
+function convert(number) {
+	var finalNumber;
+	if (number == "pi()") {
+		finalNumber = Math.PI;
+	}
+	else if (number == "exponentialGrowth()") {
+		finalNumber = Math.E;
+	}
+	else {
+		finalNumber = parseFloat(number);
+	}
+	return finalNumber;
+}
+
+var averageNumberAmount = 2;
+
+function averageAddInput() {
+	console.log("Running");
+	averageNumberAmount++;
+	$("#average-inputs").append('<input id="average-' + averageNumberAmount + '"><br>');
+}
+
+function runCommand() {
+	if (tab == "overview") {
+		var numberOne = $("#overview-1").val();
+		var numberTwo = $("#overview-2").val();
+		numberOne = convert(numberOne);
+		numberTwo = convert(numberTwo);
+		$("#result").html("Sum: " + (numberOne + numberTwo) + line + "Difference: " + (numberOne - numberTwo) + line + "Product: " + (numberOne * numberTwo) + line + "Quotient: " + (numberOne / numberTwo));
+	}
+	else if (tab == "average") {
+		var numbers = [];
+		for (var i = 0; i < averageNumberAmount; i++) {
+			numbers.push($("#average-" + i+1).val());
+		}
+		for (var i = 0; i < averageNumberAmount; i++) {
+			convert(numbers[i]);
+		}
+		var resultNumber = 0;
+		
+	}
+}
+
+function showTab(newTab) {
+	$(".tab").hide();
+	$("#tab-" + newTab).show();
+	$(".tab-button").removeClass("tab-selected");
+	$("#tab-button-" + newTab).addClass("tab-selected");
+	tab = newTab;
+}
+
+$(document).ready(function() {
+	showTab("overview");
+});
