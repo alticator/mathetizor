124fd4b9b7a8db07de0f2a0a213cad890ee0672e
Teemu Kuuva <teemu.kuuva@gmail.com>
2020-04-18 18:59:01 +0300

    Add first console test implementation, handles single numbers with sign mark

--- app.js
+++ app.js
@@ -148,6 +148,175 @@ function runCommand() {
 		$("#calculator-1").text(result);
 		$("#result").html(result);
 	}
+    else if (tab == "console") {
+        executeConsole($("#console-1").val());
+    }
+}
+
+// parse line and construct an equation
+function executeConsole(line) {
+    console.log("executeConsole: [", line, "]");
+    var equation = new Array();
+
+    // put tokens to equation
+    // start with number/operand, then expect operation/operator
+
+    var nextType = "operand";
+    do {
+        var ret = getToken(line, nextType);
+        if (ret[0])  {
+            equation.push(["operand", ret[1]]);
+            nextType = "operator";
+        }
+        else
+        {
+            console.log("executeConsole: no more tokens");
+            break;
+        }
+    } while (ret[0] == true)
+
+    calculateEquation(equation);
+}
+
+// returns true/false, token
+function getToken(text, type) {
+    //console.log("getToken: ", text, type);
+    var candidate = NaN;
+    if (type == "operand") {
+        // get the longest string that is a number
+        for (var i=1; (i-1) < text.length; i++)
+        {
+            console.log("parse operand: ", text.substring(0, i));
+            var parsedFloat = parseFloat(text.substring(0, i));
+            console.log("candidate: ", parsedFloat);
+            if (!isNaN(parsedFloat))
+            {
+                // so far the best
+                candidate = parsedFloat;
+                //console.log("getToken found number: ", candidate);
+            }
+            else
+            {
+                // not a number (anymore), stop
+                // exception: don't stop on first round,
+                // could be a sign mark (+/-) which can be followed by a number
+                if (i != 1) {
+                    break;
+                }
+            }
+        }
+    }
+
+    console.log("getToken(", text, ") result: ", candidate);
+    if (isNaN(candidate)) {
+        console.log("getToken failed");
+        return [false, ""];
+    }
+    else {
+        console.log("getToken return ", candidate);
+        return [true, candidate];
+    }
+}
+
+// handle array of operands and operations
+// make a linked list of singleOps
+// complete "vajaa" operations
+// try to process a operand + operator + operand triplet
+// if a complete singleOp not found, fill with [0, plus]
+function calculateEquation(equation) {
+    console.log("calculateEquation: ", equation);
+    
+    // construct a list of calculus operations, that are then executed in priority order
+    var operationList = []; // array of singleOps
+    
+    var singleOp =  new Array(); // [left operand, operation, right operand]
+    
+    console.log("begin singleOp: ", singleOp);
+    
+    // handle equation items [type, value]
+    for (var i=0; i < equation.length; i++) {
+        var itemType = equation[i][0];
+        var itemValue = equation[i][1];
+        var isLastItem = (i == (equation.length -1))
+        
+        console.log("eq item: ", itemType, itemValue);
+        if (itemType == "operand") {
+            console.log("calculateEquation operand");
+            if (singleOp.length == 0) {
+                //start new singleOp, add left operand
+                console.log("add left operand: ", itemValue);
+                singleOp.push(itemValue);
+            }
+            else if (singleOp.length == 2) {
+                // add right operand
+                console.log("add right operand: ", itemValue);
+                singleOp.push(itemValue);
+                operationList.push(singleOp);
+            }
+            else {
+                console.log("singleOp error 1");
+                break;
+            }
+        }
+        else if (itemType == "operator") {
+            console.log("calculateEquation operator");
+            if (singleOp.length == 1) {
+                //add operation to singleOp
+                singleOp.push(equation[i][1]);
+            }
+            else {
+                console.log("singleOp error 2");
+                break;
+            }
+        }
+        
+        // if singleOp will not be complete, fill it
+        if (singleOp.length < 3 && isLastItem) {
+            singleOp = completeOperation(singleOp);
+            operationList.push(singleOp);
+        }
+    }
+    
+    evaluateResult(operationList);
+}
+
+function completeOperation(singleOp) {
+    console.log("completeOperation: ", singleOp);
+    if (singleOp.length == 1) {
+        singleOp.unshift(0, "plus");
+    }
+    else if (singleOp.length == 2) {
+        if (isNan(singleOp[0])) {
+            singleOp.unshift(0); //0 operand number
+        }
+        else {
+            singleOp.append(0);  //number operand 0
+        }
+    }
+    console.log("after completeOperation: ", singleOp);
+    return singleOp;
+}
+
+function evaluateResult(operationList) {
+    console.log("evaluateResult: ", operationList);
+    // start with zero, that will default so somewhat reasonable result
+    var result = 0;
+    for (var n=0; n < operationList.length; n++) {
+        op = operationList[n];
+        result += evaluateOp(op);
+    }       
+ 
+    $("#result").html(result);
+}
+
+function evaluateOp(singleOp) {
+    console.log("evaluateOp: ", singleOp);
+    if (op[1] == "plus") {
+        return op[0] + op[2];
+    }
+    else if (op[1] == "minus") {
+        return op[0] - op[2];
+    }
 }
 
 function showTab(newTab) {
@@ -162,4 +331,6 @@ function showTab(newTab) {
 $(document).ready(function() {
 	showTab("calculator");
 	$(document).keypress(keyPress);
+    
+    showTab("console");
 });
